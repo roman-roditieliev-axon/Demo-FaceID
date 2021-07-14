ba0a905689e811e7e7241901cef709a75d730742
roman-roditieliev-axon <rroditieliev@axon.dev>
2021-07-14 13:46:51 +0300

    setup ui

--- Demo-FaceID/ViewControllers/LoginViewController.swift
+++ Demo-FaceID/ViewControllers/LoginViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

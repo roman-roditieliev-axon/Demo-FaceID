ff2630a37f4f24db25a3d34273ec43ce405ab3da
roman-roditieliev-axon <rroditieliev@axon.dev>
2021-07-14 14:17:21 +0300

    setup login vc

--- Demo-FaceID/ViewControllers/LoginViewController.swift
+++ Demo-FaceID/ViewControllers/LoginViewController.swift
@@ -11,12 +11,45 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupVC()
     }
 
+    private func setupVC() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+
+        loginButton.layer.cornerRadius = 5
+        emailTextField.layer.cornerRadius = 5
+        passwordTextField.layer.cornerRadius = 5
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email",
+                                                                  attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password",
+                                                                     attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+
+        emailTextField.layer.borderWidth = 2
+        passwordTextField.layer.borderWidth = 2
+        loginButton.layer.borderWidth = 2
+
+        emailTextField.layer.borderColor = UIColor.darkGray.cgColor
+        passwordTextField.layer.borderColor = UIColor.darkGray.cgColor
+        loginButton.layer.borderColor = UIColor.darkGray.cgColor
+
+    }
+
+    @IBAction func didTapLoginButton(_ sender: Any) {
+
+    }
 
 }
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+}
